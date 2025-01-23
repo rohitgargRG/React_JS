47efb4b1753f444be5a28a263202487f4358820b
rohitgargRG <gargrohit2003@gmail.com>
2025-01-23 06:29:40 +0530

    created react ourselve

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+/*
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+)
+*/
+
+const reactELement = (
+   'a',
+   {href : "https://google.com" , target : "_blank"},
+   "click me to visit google"
+)
+
+ReactDOM.createRoot(document.getElementById('root'))
+.render(
+  reactELement
 )
