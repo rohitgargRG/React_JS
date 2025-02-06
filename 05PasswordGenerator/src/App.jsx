687c8469aa78594ee0b52a95380cbab42bec105e
rohitgargRG <gargrohit2003@gmail.com>
2025-02-06 17:13:59 +0530

    completed password generator project

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState , useCallback} from 'react'
+import { useState , useCallback , useEffect , useRef} from 'react'
 
 function App() {
   
@@ -25,32 +25,102 @@ function App() {
 
       for(let i = 1 ; i <= length ; i++){
         let char = Math.floor(Math.random()*str.length + 1);
-        pass = str.charAt(char)
+        pass += str.charAt(char)
       }
 
       setPassword(pass)
   },[length , numberAllowed , characterAllowed , setPassword])
 
-  
+  // we cannot call this function like this now.
+  // passwordGenerator()
 
+  // use useEffect() hook to make the function call
+  useEffect(() => {
+    passwordGenerator()
+  } , [length , numberAllowed , characterAllowed , passwordGenerator])
+
+
+  // creating copy feature
+  // useRef() hook
+  const passwordRef = useRef(null)
+
+  const copyPasswordToClipBoard = useCallback(() => {
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0,101)
+    window.navigator.clipboard.writeText(password)
+  } , [password])
 
 
   return (
     <>
+    <div className='flex justify-center items-center w-[100vw] h-[100vh]'>
+      <div className='md:w-[80%]  lg:w-[50%]  w- max-h-md mx-auto shadow-md rounded-lg px-4 py-4 my-8 text-orange-500 bg-gray-800'>
+        <p className='text-white text-center my-3 text-5xl'>Password Generator</p>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+            
+            <input 
+            type="text" 
+            value = {password}
+            className='outline-none w-full py-1 px-3 bg-white text-gray-600'
+            placeholder='Password'
+            readOnly
+            // use ref to get reference of the value written inside this field.
+            ref = {passwordRef}
+            />
 
-    <div className='w-full max-h-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800 '>
-      <h1 className='text-white text-center my-3 '>Password Generator</h1>
-      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-          <input 
-          type="text" 
-          value = {password}
-          className='outline-none w-full py-1 px-3 bg-white text-gray-600'
-          placeholder='Password'
-          readOnly
-          />
+            <button
+            onClick = {copyPasswordToClipBoard}
+            className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
 
-      </div>
+        </div>
 
+        <div className='flex text-sm gap-x-8'>
+          {/* // slider */}
+          <div className='flex items-center gap-x-1'>
+                <input 
+                  type="range" 
+                  min = {6} 
+                  max = {100} 
+                  value = {length} 
+                  className='cursor-pointer' 
+                  onChange={
+                      (e) => {setLength(e.target.value)
+                  }}
+                />
+                <label>Length: {length}</label>
+          </div>
+
+          {/* // numbers checkbox */}
+          <div className='flex items-center gap-x-1'>
+                <input 
+                  type="checkbox" 
+                  defaultChecked = {numberAllowed}
+                  id='numberInput'
+                  onChange={
+                       (e) => {setNumberAllowed((prev) => !prev)
+                  }}
+                />
+                <label htmlFor='numberInput'>Numbers</label>
+          </div>
+
+          {/* // characters checkbox */}
+          <div className='flex items-center gap-x-1'>
+                <input 
+                  type="checkbox" 
+                  defaultChecked = {characterAllowed}
+                  id='characterInput'
+                  onChange={
+                       (e) => {setcharacterAllowed((prev) => !prev)
+                  }}
+                />
+                <label htmlFor='characterInput'>Characters</label>
+          </div>
+
+
+        </div>
+
+
+    </div>
 
     </div>
 
