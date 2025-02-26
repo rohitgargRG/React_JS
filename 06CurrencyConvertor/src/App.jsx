ee92bab73a1f6698b3dba26a9a688860ad387dd8
rohitgargRG <gargrohit2003@gmail.com>
2025-02-26 07:15:05 +0530

    resuming

---
+++ 06CurrencyConvertor/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-orange-500'>cr7</h1>
+    </>
+  )
+}
+
+export default App
